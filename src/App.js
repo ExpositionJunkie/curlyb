ec5a3e1eeddc21a291e946e63017d4854fabf50c
Roz Albrecht <rosalindavalbrecht@gmail.com>
2022-02-23 14:11:22 -0800

    minor changes added title and home page templates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { useState, Suspense, useEffect } from "react";
 import "./App.css";
-
+import { Home } from "./Components/Home/Home";
 import Language from "./Components/Reusable/Language/Language";
 import Loading from "./Components/Reusable/Loading/Loading";
 import Greeting from "./Components/Reusable/Greeting/Greeting";
 import Paragraph from "./Components/Reusable/Paragraph/Paragraph";
 import i18n from "./Language/i18n";
 import LocaleContext from "./LocaleContext";
+
+//Need to read
+//https://mattermost.com/blog/avoiding-common-internationalization-mistakes/
+
+//Possibly good when I start setting up the node server but isn't quite what I was hoping for here.
+//https://johnresig.com/blog/a-strategy-for-i18n-and-node/
 
 function App() {
   const [locale, setLocale] = useState(i18n.language);
@@ -17,8 +23,8 @@ function App() {
 
   return (
     <LocaleContext.Provider value={{ locale, setLocale }}>
-      <Suspense fallback={<p>...Loading</p>}>
-        <div className="App">
+      <div className="App">
+        <Suspense fallback={<p>...Loading</p>}>
           <header className="App-header">
             <div>
               <Greeting />
@@ -28,8 +34,8 @@ function App() {
               <Language />
             </Suspense>
           </header>
-        </div>
-      </Suspense>
+        </Suspense>
+      </div>
     </LocaleContext.Provider>
   );
 }
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export function Home(props) {
+    
+
+    return (
+        <>
+            <h1>Curly Brackets</h1>
+        </>
+    )
+}
