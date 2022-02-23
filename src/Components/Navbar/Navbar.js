399bec703aff3eb83fa9ba7130396dbb145472f1
Roz Albrecht <rosalindavalbrecht@gmail.com>
2022-02-22 19:59:18 -0800

    minor cleanup and moved language dropdown to its own component. Can prob put in the footer as a dropdown and in the header as an icon later

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,39 +1,11 @@
-import React, { useState, useEffect } from "react";
-import { supportedLanguages } from "../../data/supportedLanguage";
-
-export function NavbarHelper({ languages }) {
-  if (languages !== typeof undefined) {
-    languages.map((language) => {
-      return (
-        <option id={language.id} value={language.langCode}>
-          {language.name}
-        </option>
-      );
-    });
-  } else {
-    return null;
-  }
-}
+import React from "react";
+import Language from "../Reusable/Language/Language"
 
 export default function Navbar() {
-  const [languages, setLanguages] = useState([...supportedLanguages]);
-  const [isLoading, setIsLoading] = useState(true);
 
   return (
     <div>
-      <form>
-        <select id="language">
-          <option value="EN">Choose a language</option>
-          {languages.map((language) => {
-            return (
-              <option id={language.id} value={language.langCode}>
-                {language.name}
-              </option>
-            );
-          })}
-        </select>
-        <input type="submit" value="Submit"></input>
-      </form>
+        <Language />
     </div>
   );
 }
--- src/Components/Reusable/Language/Language.js
+++ src/Components/Reusable/Language/Language.js
@@ -1,1 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { supportedLanguages } from "../../data/supportedLanguage";
+
+export default function Language() {
+  const [languages, setLanguages] = useState([...supportedLanguages]);
+
+  return (
+    <div>
+      <form>
+        <select id="language">
+          <option value="EN">Choose a language</option>
+          {languages.map((language) => {
+            return (
+              <option id={language.id} value={language.langCode}>
+                {language.name}
+              </option>
+            );
+          })}
+        </select>
+        <input type="submit" value="Submit"></input>
+      </form>
+    </div>
+  );
+}
