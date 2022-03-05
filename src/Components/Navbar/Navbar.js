7df9b673231250edef012ea65786ad7505277aae
Roz Albrecht <rosalindavalbrecht@gmail.com>
2022-03-04 17:54:21 -0800

    updated navbar scss

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,34 +1,23 @@
 import React from "react";
 import { NavLink, Outlet } from "react-router-dom";
-import banner from "../../data/photos/dragons-song-colorful-banner.jpg";
-
 import "./Navbar.css";
 
 export default function Navbar() {
   return (
     <div className="navbar">
-      <div className="headerTitle">
-        <div className="headerOverlay">
-          <img className="banner"
-            src={banner}
-            alt="Colorful banner resembling petals or dragon scales created by NightCafe - a website specializing in art made by artificial intelligence."
-          />
-          <h1 className="titleText">Curly Brackets</h1>
-        </div>
+      <div className="headerWrapper">
+        <h1 className="titleText">Curly Brackets</h1>
       </div>
-
       <div className="linkWrapper">
-        <div>
-          <span className="navbarLinks">
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/blog">Blog</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/apipractice">API Practice</NavLink>
-            <NavLink to="/signup">Sign Up</NavLink>
-          </span>
-        </div>
-        <Outlet />
+        <span className="navbarLinks">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/blog">Blog</NavLink>
+          <NavLink to="/about">About</NavLink>
+          <NavLink to="/apipractice">API Practice</NavLink>
+          <NavLink to="/signup">Sign Up</NavLink>
+        </span>
       </div>
+      <Outlet />
     </div>
   );
 }
