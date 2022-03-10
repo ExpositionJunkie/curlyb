986dfeccb26ab7f55dea7463dd0019d0118cd5a0
Roz Albrecht <rosalindavalbrecht@gmail.com>
2022-03-10 14:52:13 -0800

    testing deploy

--- src/Components/APIPractice/APIPractice.js
+++ src/Components/APIPractice/APIPractice.js
@@ -9,26 +9,29 @@ export default function APIPractice() {
       <Title titleStr="Api Practice List" />
       <div className="innerWrapper pad4">
         <div className="apiExplanation">
+          <h1>TEST TO SEE IF DEPLOY WORKING CORRECTLY</h1>
           <h3>List of various API practice pages aggregated here.</h3>
           <h4>
             If you want to see what got completed during what weeks, check out
             my update page or dev blog for an experience of the process
           </h4>
         </div>
-      <ul className="linkNoUnderline">
-        <li>
-          <NavLink to="/nameguesser">Age Guesser</NavLink> (Complete 2/15/2022)
-        </li>
-        <li>
-          <NavLink to="/nasapower">NASA Power Albedo API </NavLink>(Complete
-          2/17/2022)
-        </li>
-        <li>
-          <NavLink to="/isslocator">ISS Locator</NavLink>(Complete 2/17/2022)
-        </li>
-        <li>
-          <NavLink to="/dnd">DND Spells List Search</NavLink>(Complete 2/18/2022)
-        </li>
+        <ul className="linkNoUnderline">
+          <li>
+            <NavLink to="/nameguesser">Age Guesser</NavLink> (Complete
+            2/15/2022)
+          </li>
+          <li>
+            <NavLink to="/nasapower">NASA Power Albedo API </NavLink>(Complete
+            2/17/2022)
+          </li>
+          <li>
+            <NavLink to="/isslocator">ISS Locator</NavLink>(Complete 2/17/2022)
+          </li>
+          <li>
+            <NavLink to="/dnd">DND Spells List Search</NavLink>(Complete
+            2/18/2022)
+          </li>
         </ul>
       </div>
     </div>
