3090709535725cb0bcee13f27b99323beb7a2347
Roz Albrecht <rosalindavalbrecht@gmail.com>
2022-03-10 14:30:07 -0800

    update to signup

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,36 +1,9 @@
 import React, { useState } from "react";
-import axios from "axios";
 
 export default function Signup(props) {
   const [input, setInput] = useState({ email: "", username: "", password: "" });
   const [error, setError] = useState(null);
 
-  const handleSubmit = (evt) => {
-    var url = "https://localhost:2443/users/signup";
-    axios
-      .post(url, {
-        username: `${input.username}`,
-        email: `${input.email}`,
-        password: `${input.password}`,
-      })
-      .then((response) => {
-        response.json();
-        console.log(response);
-      })
-      .then((data, err) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log("success!", data);
-        }
-      })
-      .catch((err) => {
-        setError({ errorMessage: err.toString() });
-        console.error("There was an error!", error);
-      });
-
-    evt.preventDefault();
-  };
 
   const handleChange = (evt) => {
     setInput((prevState) => ({
@@ -45,7 +18,7 @@ export default function Signup(props) {
       <div className="signupWrapper">
         <h1>Sign Up</h1>
         <div className="signupForm">
-          <form onSubmit={(e) => handleSubmit(e)}>
+          <form>
             <label htmlFor="email">Email</label>
             <input
               type="text"
