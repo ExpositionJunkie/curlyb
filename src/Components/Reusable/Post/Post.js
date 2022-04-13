3c01f74756a1c6dc549b7ed59ea892ccb5eb555a
Roz Albrecht <rosalindavalbrecht@gmail.com>
2022-04-13 16:37:28 -0700

    tweaking formatting, will need to come up with something better for small screens

--- src/Components/Reusable/Post/Post.js
+++ src/Components/Reusable/Post/Post.js
@@ -27,10 +27,10 @@ function Post(props) {
         autocomplete="none"
         placeholder="Subtitle"
       ></input>
-      <EditorButtons editor={editor}></EditorButtons>
       <div className="editor">
         <EditorContent editor={editor} />
       </div>
+      <EditorButtons editor={editor}></EditorButtons>
       <button className="submit-button">
         This button does not do anything yet.
       </button>
@@ -45,215 +45,280 @@ function EditorButtons({ editor }) {
 
   return (
     <div className="editor-button-wrap">
-      <div className="tooltip sm-row-1">
-        <button
-          onClick={() => editor.chain().focus().toggleBold().run()}
-          className={editor.isActive("bold") ? "is-active" : ""}
-        >
-          <FontAwesomeIcon icon={solid("bold")} />
-        </button>
-        <span className="tooltip-text sm-row-1">Bold</span>
-      </div>
-
-      <div className="tooltip">
-        <button
-          onClick={() => editor.chain().focus().toggleItalic().run()}
-          className={editor.isActive("italic") ? "is-active" : ""}
-        >
-          <FontAwesomeIcon icon={solid("italic")} />
-        </button>
-        <span className="tooltip-text sm-row-1">Italic</span>
-      </div>
-
-      <div className="tooltip">
-        <button
-          onClick={() => editor.chain().focus().toggleStrike().run()}
-          className={editor.isActive("strike") ? "is-active" : ""}
-        >
-          <FontAwesomeIcon icon={solid("strikethrough")} />
-        </button>
-        <span className="tooltip-text sm-row-1">Strikethrough</span>
-      </div>
-
-      <div className="tooltip">
-        <button onClick={() => editor.chain().focus().unsetAllMarks().run()}>
-          <FontAwesomeIcon icon={solid("eraser")} />
-        </button>
-        <span className="tooltip-text sm-row-1">Remove Formatting</span>
-      </div>
-
-      <div className="tooltip">
+      <div className="tooltip small-formatting-hide">
         <button
           onClick={() => editor.chain().focus().setParagraph().run()}
-          className={editor.isActive("paragraph") ? "is-active" : ""}
+          className={
+            editor.isActive("paragraph")
+              ? "is-active editor-button"
+              : "editor-button"
+          }
         >
           <FontAwesomeIcon icon={solid("paragraph")} />
         </button>
-        <span className="tooltip-text sm-row-1">Paragraph</span>
+        <span className="tooltip-text shadow-box-no-zoom">Paragraph</span>
       </div>
-
-      <div className="tooltip">
+      <div className="tooltip small-formatting-hide">
         <button
           onClick={() =>
             editor.chain().focus().toggleHeading({ level: 1 }).run()
           }
           className={
-            editor.isActive("heading", { level: 1 }) ? "is-active" : ""
+            editor.isActive("heading", { level: 1 })
+              ? "is-active editor-button"
+              : "editor-button"
           }
         >
           H1
         </button>
-        <span className="tooltip-text">Heading 1</span>
+        <span className="tooltip-text shadow-box-no-zoom ">Heading 1</span>
       </div>
-
-      <div className="tooltip sm-row-1">
+      <div className="tooltip small-formatting-hide">
         <button
           onClick={() =>
             editor.chain().focus().toggleHeading({ level: 2 }).run()
           }
           className={
-            editor.isActive("heading", { level: 2 }) ? "is-active" : ""
+            editor.isActive("heading", { level: 2 })
+              ? "is-active editor-button"
+              : "editor-button"
           }
         >
           H2
         </button>
-        <span className="tooltip-text">Heading 2</span>
+        <span className="tooltip-text shadow-box-no-zoom small-formatting-hide">
+          Heading 2
+        </span>
       </div>
 
-      <div className="tooltip sm-row-1">
+      <div className="tooltip small-formatting-hide">
         <button
           onClick={() =>
             editor.chain().focus().toggleHeading({ level: 3 }).run()
           }
           className={
-            editor.isActive("heading", { level: 3 }) ? "is-active" : ""
+            editor.isActive("heading", { level: 3 })
+              ? "is-active editor-button"
+              : "editor-button"
           }
         >
           H3
         </button>
-        <span className="tooltip-text">Heading 3</span>
+        <span className="tooltip-text shadow-box-no-zoom">Heading 3</span>
       </div>
 
-      <div className="tooltip sm-row-1">
+      <div className="tooltip small-formatting-hide">
         <button
           onClick={() =>
             editor.chain().focus().toggleHeading({ level: 4 }).run()
           }
           className={
-            editor.isActive("heading", { level: 4 }) ? "is-active" : ""
+            editor.isActive("heading", { level: 4 })
+              ? "is-active editor-button"
+              : "editor-button"
           }
         >
           H4
         </button>
-        <span className="tooltip-text">Heading 4</span>
+        <span className="tooltip-text shadow-box-no-zoom ">Heading 4</span>
       </div>
 
-      <div className="tooltip sm-row-1">
+      <div className="tooltip small-formatting-hide">
         <button
           onClick={() =>
             editor.chain().focus().toggleHeading({ level: 5 }).run()
           }
           className={
-            editor.isActive("heading", { level: 5 }) ? "is-active" : ""
+            editor.isActive("heading", { level: 5 })
+              ? "is-active editor-button"
+              : "editor-button"
           }
         >
           H5
         </button>
-        <span className="tooltip-text">Heading 5</span>
+        <span className="tooltip-text shadow-box-no-zoom">Heading 5</span>
       </div>
 
-      <div className="tooltip sm-row-1">
+      <div className="tooltip small-formatting-hide">
         <button
           onClick={() =>
             editor.chain().focus().toggleHeading({ level: 6 }).run()
           }
           className={
-            editor.isActive("heading", { level: 6 }) ? "is-active" : ""
+            editor.isActive("heading", { level: 6 })
+              ? "is-active editor-button"
+              : "editor-button"
           }
         >
           H6
         </button>
-        <span className="tooltip-text">Heading 6</span>
+        <span className="tooltip-text shadow-box-no-zoom">Heading 6</span>
       </div>
-
-      <div className="tooltip sm-row-1">
+      <div className="tooltip">
         <button
-          onClick={() => editor.chain().focus().toggleBulletList().run()}
-          className={editor.isActive("bulletList") ? "is-active" : ""}
+          className="editor-button"
+          onClick={() => editor.chain().focus().unsetAllMarks().run()}
         >
-          <FontAwesomeIcon icon={solid("list")} />
+          <FontAwesomeIcon icon={solid("eraser")} />
         </button>
-        <span className="tooltip-text">Bullet Point List</span>
-      </div>
-
-      <div className="tooltip sm-row-2">
-        <button
-          onClick={() => editor.chain().focus().toggleOrderedList().run()}
-          className={editor.isActive("orderedList") ? "is-active " : ""}
-        >
-          <FontAwesomeIcon icon={solid("list-ol")} />
-        </button>
-        <span className="tooltip-text">Numbered/Ordered List</span>
-      </div>
-
-      <div className="tooltip sm-row-2">
-        <button
-          onClick={() => editor.chain().focus().toggleCodeBlock().run()}
-          className={editor.isActive("codeBlock") ? "is-active " : ""}
-        >
-          <FontAwesomeIcon icon={solid("file-code")} />
-        </button>
-        <span className="tooltip-text">Code Block</span>
-      </div>
-
-      <div className="tooltip sm-row-2">
-        <button
-          onClick={() => editor.chain().focus().toggleCode().run()}
-          className={editor.isActive("code") ? "is-active " : ""}
-        >
-          <FontAwesomeIcon icon={solid("code")} />
-        </button>
-        <span className="tooltip-text">Code Snippet</span>
-      </div>
-
-      <div className="tooltip sm-row-2">
-        <button
-          onClick={() => editor.chain().focus().toggleBlockquote().run()}
-          className={editor.isActive("blockquote") ? "is-active " : ""}
-        >
-          <FontAwesomeIcon icon={solid("quote-left")} />
-        </button>
-        <span className="tooltip-text">Blockquote</span>
+        <span className="tooltip-text shadow-box-no-zoom">
+          Remove Formatting
+        </span>
       </div>
 
       <div className="tooltip">
         <button
+          onClick={() => editor.chain().focus().toggleBold().run()}
+          className={
+            editor.isActive("bold")
+              ? "is-active editor-button"
+              : "editor-button"
+          }
+        >
+          <FontAwesomeIcon icon={solid("bold")} />
+        </button>
+        <span className="tooltip-text shadow-box-no-zoom">Bold</span>
+      </div>
+
+      <div className="tooltip">
+        <button
+          onClick={() => editor.chain().focus().toggleItalic().run()}
+          className={
+            editor.isActive("italic")
+              ? "is-active editor-button"
+              : "editor-button"
+          }
+        >
+          <FontAwesomeIcon icon={solid("italic")} />
+        </button>
+        <span className="tooltip-text shadow-box-no-zoom">Italic</span>
+      </div>
+
+      <div className="tooltip">
+        <button
+          onClick={() => editor.chain().focus().toggleStrike().run()}
+          className={
+            editor.isActive("strike")
+              ? "is-active editor-button"
+              : "editor-button"
+          }
+        >
+          <FontAwesomeIcon icon={solid("strikethrough")} />
+        </button>
+        <span className="tooltip-text shadow-box-no-zoom">Strikethrough</span>
+      </div>
+
+      <div className="tooltip">
+        <button
+          onClick={() => editor.chain().focus().toggleBulletList().run()}
+          className={
+            editor.isActive("bulletList")
+              ? "is-active editor-button"
+              : "editor-button"
+          }
+        >
+          <FontAwesomeIcon icon={solid("list")} />
+        </button>
+        <span className="tooltip-text shadow-box-no-zoom">
+          Bullet Point List
+        </span>
+      </div>
+
+      <div className="tooltip">
+        <button
+          onClick={() => editor.chain().focus().toggleOrderedList().run()}
+          className={
+            editor.isActive("orderedList")
+              ? "is-active editor-button"
+              : "editor-button"
+          }
+        >
+          <FontAwesomeIcon icon={solid("list-ol")} />
+        </button>
+        <span className="tooltip-text shadow-box-no-zoom">
+          Numbered/Ordered List
+        </span>
+      </div>
+
+      <div className="tooltip">
+        <button
+          onClick={() => editor.chain().focus().toggleCodeBlock().run()}
+          className={
+            editor.isActive("codeBlock")
+              ? "is-active editor-button"
+              : "editor-button"
+          }
+        >
+          <FontAwesomeIcon icon={solid("file-code")} />
+        </button>
+        <span className="tooltip-text shadow-box-no-zoom">Code Block</span>
+      </div>
+
+      <div className="tooltip">
+        <button
+          onClick={() => editor.chain().focus().toggleCode().run()}
+          className={
+            editor.isActive("code")
+              ? "is-active editor-button"
+              : "editor-button"
+          }
+        >
+          <FontAwesomeIcon icon={solid("code")} />
+        </button>
+        <span className="tooltip-text shadow-box-no-zoom">Code Snippet</span>
+      </div>
+
+      <div className="tooltip">
+        <button
+          onClick={() => editor.chain().focus().toggleBlockquote().run()}
+          className={
+            editor.isActive("blockquote")
+              ? "is-active editor-button"
+              : "editor-button"
+          }
+        >
+          <FontAwesomeIcon icon={solid("quote-left")} />
+        </button>
+        <span className="tooltip-text shadow-box-no-zoom">Blockquote</span>
+      </div>
+
+      <div className="tooltip">
+        <button
+          className="editor-button"
           onClick={() => editor.chain().focus().setHorizontalRule().run()}
         >
           <FontAwesomeIcon icon={solid("ruler-horizontal")} />
         </button>
-        <span className="tooltip-text">Horizontal Line</span>
+        <span className="tooltip-text shadow-box-no-zoom">Horizontal Line</span>
       </div>
 
-      <div className="tooltip sm-row-2">
-        <button onClick={() => editor.chain().focus().setHardBreak().run()}>
+      <div className="tooltip">
+        <button
+          className="editor-button"
+          onClick={() => editor.chain().focus().setHardBreak().run()}
+        >
           <FontAwesomeIcon icon={solid("arrow-down")} />
         </button>
-        <span className="tooltip-text">New Line</span>
+        <span className="tooltip-text shadow-box-no-zoom">New Line</span>
       </div>
 
-      <div className="tooltip sm-row-2">
-        <button onClick={() => editor.chain().focus().undo().run()}>
+      <div className="tooltip">
+        <button
+          className="editor-button"
+          onClick={() => editor.chain().focus().undo().run()}
+        >
           <FontAwesomeIcon icon={solid("rotate-left")} />
         </button>
-        <span className="tooltip-text">Undo</span>
+        <span className="tooltip-text shadow-box-no-zoom">Undo</span>
       </div>
-      <div className="tooltip sm-row-2">
-        <button onClick={() => editor.chain().focus().redo().run()}>
+      <div className="tooltip">
+        <button
+          className="editor-button"
+          onClick={() => editor.chain().focus().redo().run()}
+        >
           <FontAwesomeIcon icon={solid("rotate-right")} />
         </button>
-
-        <span className="tooltip-text">Redo</span>
+        <span className="tooltip-text shadow-box-no-zoom">Redo</span>
       </div>
     </div>
   );
