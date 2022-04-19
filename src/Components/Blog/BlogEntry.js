2344dc29a98584f75185899ca959b10477c573d8
Roz Albrecht <rosalindavalbrecht@gmail.com>
2022-04-19 12:10:33 -0700

    formatting further

--- src/Components/Blog/BlogEntry.js
+++ src/Components/Blog/BlogEntry.js
@@ -44,7 +44,7 @@ export default function BlogEntry({ blog }) {
             );
           })}
         </div>
-
+        
       </div>
     );
   } else {
--- src/Components/Blog/BlogEntryStandalone.js
+++ src/Components/Blog/BlogEntryStandalone.js
@@ -67,6 +67,7 @@ export function BlogEntryS() {
             );
           })}
         </div>
+
       </div>
     );
   } else {
