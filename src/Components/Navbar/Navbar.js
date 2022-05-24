e8d1f25506685afbb0da71e2a74239847197f7e5
Roz Albrecht <rosalindavalbrecht@gmail.com>
2022-05-24 15:25:23 -0700

    updated navbar now sticks to top. :)

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -13,110 +13,134 @@ export default function Navbar({ auth }) {
 
   return (
     <nav className="navbar">
-      
       <NavLink className="nav-link-text-deco" to="/">
         <Title
           titleStr="Curly Brackets"
           centerLogo={<Logo className="logo" />}
         ></Title>
       </NavLink>
+      <div className="sticky-div">
+        {isNavExpanded ? ( //terinary to see if the menu is open
+          <div className="menu-dropdown">
+            {auth.isAuthenticated ? ( //show login/logout dynamically
+              <NavLink
+                onClick={() => setIsNavExpanded(!isNavExpanded)}
+                className="menu-item"
+                to="/login"
+              >
+                <FontAwesomeIcon
+                  className="nav-icon"
+                  icon={solid("user-astronaut")}
+                />
+                Logout
+              </NavLink>
+            ) : (
+              <NavLink
+                onClick={() => setIsNavExpanded(!isNavExpanded)}
+                className="menu-item"
+                to="/login"
+              >
+                <FontAwesomeIcon
+                  className="nav-icon"
+                  icon={solid("user-astronaut")}
+                />
+                Login
+              </NavLink>
+            )}
+            <NavLink
+              onClick={() => setIsNavExpanded(!isNavExpanded)}
+              className="menu-item"
+              to="/"
+            >
+              <FontAwesomeIcon className="nav-icon" icon={solid("home")} />
+              Home
+            </NavLink>
+            <NavLink
+              onClick={() => setIsNavExpanded(!isNavExpanded)}
+              className="menu-item"
+              to="/blog"
+            >
+              <FontAwesomeIcon className="nav-icon" icon={solid("blog")} />
+              Blog
+            </NavLink>
+            <NavLink
+              onClick={() => setIsNavExpanded(!isNavExpanded)}
+              className="menu-item"
+              to="/about"
+            >
+              <FontAwesomeIcon
+                className="nav-icon"
+                icon={solid("address-card")}
+              />
+              About
+            </NavLink>
+            <NavLink
+              onClick={() => setIsNavExpanded(!isNavExpanded)}
+              className="menu-item"
+              to="/blog/devblog"
+            >
+              <FontAwesomeIcon className="nav-icon" icon={solid("computer")} />
+              Devblog
+            </NavLink>
+            <NavLink
+              onClick={() => setIsNavExpanded(!isNavExpanded)}
+              className="menu-item"
+              to="/csp"
+            >
+              <FontAwesomeIcon
+                className="nav-icon"
+                icon={solid("cookie-bite")}
+              />
+              Cookie Policy
+            </NavLink>
+          </div>
+        ) : (
+          <></>
+        )}
 
-      
-      {isNavExpanded ? ( //terinary to see if the menu is open
-        <div className="menu-dropdown" >
-          {auth.isAuthenticated ? ( //show login/logout dynamically
-            <NavLink
+        <div className="linkWrapper">
+          <span className="navbarLinks">
+            <button
+              className="hamburger"
               onClick={() => setIsNavExpanded(!isNavExpanded)}
-              className="menu-item"
-              to="/login"
             >
-              <FontAwesomeIcon className="nav-icon" icon={solid("user-astronaut")} />
-              Logout
+              <FontAwesomeIcon icon={solid("bars")} />
+            </button>
+
+            <NavLink to="/">
+              <FontAwesomeIcon className="nav-icon" icon={solid("home")} />
+              <div className="long-menu">Home</div>
             </NavLink>
-          ) : (
-            <NavLink
-              onClick={() => setIsNavExpanded(!isNavExpanded)}
-              className="menu-item"
-              to="/login"
-            >
-              <FontAwesomeIcon className="nav-icon" icon={solid("user-astronaut")} />
-              Login
+            <NavLink to="/blog">
+              <FontAwesomeIcon className="nav-icon" icon={solid("blog")} />
+              <div className="long-menu">Blog</div>
             </NavLink>
-          )}
-          <NavLink
-            onClick={() => setIsNavExpanded(!isNavExpanded)}
-            className="menu-item"
-            to="/"
-          >
-            <FontAwesomeIcon className="nav-icon" icon={solid("home")} />
-            Home
-          </NavLink>
-          <NavLink
-            onClick={() => setIsNavExpanded(!isNavExpanded)}
-            className="menu-item"
-            to="/blog"
-          >
-            <FontAwesomeIcon className="nav-icon" icon={solid("blog")} />
-            Blog
-          </NavLink>
-          <NavLink
-            onClick={() => setIsNavExpanded(!isNavExpanded)}
-            className="menu-item"
-            to="/about"
-          >
-            <FontAwesomeIcon className="nav-icon" icon={solid("address-card")} />
-            About
-          </NavLink>
-          <NavLink
-            onClick={() => setIsNavExpanded(!isNavExpanded)}
-            className="menu-item"
-            to="/blog/devblog"
-           
-          >
-            <FontAwesomeIcon className="nav-icon" icon={solid("computer")} />
-            Devblog
-          </NavLink>
-          <NavLink
-            onClick={() => setIsNavExpanded(!isNavExpanded)}
-            className="menu-item"
-            to="/csp"
-          >
-            <FontAwesomeIcon className="nav-icon" icon={solid("cookie-bite")} />
-            Cookie Policy
-          </NavLink>
+            <NavLink to="/about">
+              <FontAwesomeIcon
+                className="nav-icon"
+                icon={solid("address-card")}
+              />
+              <div className="long-menu">About</div>
+            </NavLink>
+            {auth.isAuthenticated ? ( //show login/logout dynamically
+              <NavLink className="menu-item" to="/login">
+                <FontAwesomeIcon
+                  className="nav-icon"
+                  icon={solid("user-astronaut")}
+                />
+                <div className="long-menu">Logout</div>
+              </NavLink>
+            ) : (
+              <NavLink className="menu-item" to="/login">
+                <FontAwesomeIcon
+                  className="nav-icon"
+                  icon={solid("user-astronaut")}
+                />
+                <div className="long-menu">Login</div>
+              </NavLink>
+            )}
+          </span>
         </div>
-      ) : (
-        <></>
-      )}
-
-<div className="linkWrapper">
-
-
-        <span className="navbarLinks">
-        <button
-          className="hamburger"
-          onClick={() => setIsNavExpanded(!isNavExpanded)}
-        >
-          <FontAwesomeIcon icon={solid("bars")} />
-        </button>
-
-        
-          <NavLink className="long-menu" to="/"><FontAwesomeIcon className="nav-icon" icon={solid("home")} />Home</NavLink>
-          <NavLink className="long-menu" to="/blog"><FontAwesomeIcon className="nav-icon" icon={solid("blog")} />Blog</NavLink>
-          <NavLink className="long-menu" to="/about"><FontAwesomeIcon className="nav-icon" icon={solid("address-card")} />About</NavLink>
-          {auth.isAuthenticated ? ( //show login/logout dynamically
-            <NavLink className="menu-item" to="/login">
-              <FontAwesomeIcon className="nav-icon" icon={solid("user-astronaut")} />
-              Logout
-            </NavLink>
-          ) : (
-            <NavLink className="menu-item" to="/login">
-              <FontAwesomeIcon className="nav-icon" icon={solid("user-astronaut")} />
-              Login
-            </NavLink>
-          )}
-   
-        </span>
       </div>
       <Outlet />
     </nav>
